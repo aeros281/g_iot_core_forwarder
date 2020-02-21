f32773d9781ae01c0b630672ecd3ae6db648a926
Trần Lê Tuấn Anh <tranletuananh1992@gmail.com>
2020-02-21 12:18:36 +0700

    Create cli interface (#1)
    * Generate cookiecutter cli template

---
+++ src/iot_fwd/__main__.py
@@ -0,0 +1,16 @@
+""" Main application entry point.
+
+    python -m iot_fwd  ...
+
+"""
+def main():
+    """ Execute the application.
+
+    """
+    raise NotImplementedError
+
+
+# Make the script executable.
+
+if __name__ == "__main__":
+    raise SystemExit(main())
